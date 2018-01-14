4874c71160cf9e3d6dcc792c4c181106538c42c9
Valentin <harrypotter9173656@gmail.com>
2018-01-14 20:32:35 +0300

    Task4 for checking

---
+++ Hw1Task4/Hw1Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // перед запуском создать файл log.txt на диске D
+            LogMessage sw = LogMessage.GetWriter();
+            sw.Write("Helo");
+            sw.Write("How are you?");
+            LogMessage sw2 = LogMessage.GetWriter();
+            // запись в тот же файл
+            sw2.Write("I am Ok");
+        }
+    }
+}
