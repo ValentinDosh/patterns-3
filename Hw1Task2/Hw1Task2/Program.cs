034b9a96b95124a9db20e14ac5906d553c778f32
Valentin <harrypotter9173656@gmail.com>
2018-01-14 20:29:22 +0300

    Task2 for checking

---
+++ Hw1Task2/Hw1Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFormBuilder builder = new TaxFreeBuilder();
+            TaxFreeForm form1 = builder.BuidNumberPassport("12345678").BuidName("Дощечко " +
+"Валентин Виктоович").BuidAdress("Минск, Бельского 24-35").BuidIndex("220018").
+
+ BuidCountry("Беларусь").BuidEmail("Chikitan@gmail.com").GetTaxForm();
+            
+            form1.GetForm();
+        }
+    }
+}
